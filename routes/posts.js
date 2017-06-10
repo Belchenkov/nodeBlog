cb872eed8b4b8baaf161d2a09e3d93419c1636ab
Aleksey <u608110@gmai.com>
2017-06-10 12:14:44 +0300

    Add Comments

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: './public/images' });
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
+
+router.get('/show/:id', function(req, res, next) {
+    var posts = db.get('posts');
+
+    posts.findById(req.params.id, function(err, post){
+        res.render('show', {
+            'post': post
+        });
+    });
+});
+
 
 router.get('/add', function(req, res, next) {
     var categories = db.get('categories');
@@ -58,6 +69,58 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
                 req.flash('success','Post Added');
                 res.location('/');
                 res.redirect('/');
+            }
+        });
+    }
+});
+
+router.post('/addcomment', function(req, res, next) {
+    // Get Form Values
+    var postid = req.body.postid;
+    var name = req.body.name;
+    var email= req.body.email;
+    var body = req.body.body;
+    var commnetdate = new Date();
+
+    // Form Validation
+    req.checkBody('name','Name field is required').notEmpty();
+    req.checkBody('email','Email field is required but never displayed').notEmpty();
+    req.checkBody('email','Email field is not formatted properly').isEmail();
+    req.checkBody('body', 'Body field is required').notEmpty();
+
+    // Check Errors
+    var errors = req.validationErrors();
+
+    if(errors){
+        var posts = db.get('posts');
+        posts.findById('postid', (err, post) => {
+            res.render('show',{
+                "errors": errors,
+                "post": post
+            });
+        });
+    } else {
+        var comment = {
+            "name": name,
+            "email": email,
+            "body": body,
+            "commnetdate": commnetdate,
+        };
+
+        var posts = db.get('posts');
+        posts.update({
+            "_id": postid
+        }, {
+            $push: {
+                "comments": comment
+            }
+        }, (err, doc) => {
+            if (err) {
+                throw err;
+            } else {
+                req.flash('success', 'Comment Added');
+                res.location('/posts/show/' + postid);
+                res.redirect('/posts/show/' + postid);
             }
         });
     }
